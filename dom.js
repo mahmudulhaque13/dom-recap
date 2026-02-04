bc70a469191c408325fd60510e19d7df46654728
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-02-04 16:00:56 +0600

    add item

---
+++ dom.js
@@ -0,0 +1,28 @@
+// ==========================
+// DOM Element ধরছি
+// ==========================
+const input = document.getElementById("inputText"); // Input field
+const list = document.getElementById("itemList"); // UL list container
+
+// Buttons ধরছি
+const addBtn = document.getElementById("addBtn"); // Add Item
+const removeBtn = document.getElementById("removeBtn"); // Remove Last
+const changeHTMLBtn = document.getElementById("changeHTMLBtn"); // innerHTML
+const changeStyleBtn = document.getElementById("changeStyleBtn"); // Change Style
+
+// ==========================
+// Add new item
+// ==========================
+
+addBtn.onclick = () => {
+  const text = input.value.trim();
+  if (!text) return;
+
+  // নতুন <li> তৈরি
+  const li = document.createElement("li");
+  li.textContent = text;
+  li.className =
+    "p-6 bg-purple-100 rounded-2xl shadow-lg hover:shadow-2xl transition cursor-pointer text-xl"; // CSS class"
+  list.appendChild(li);
+  input.value = "";
+};
